250b90991db761bd6077cc5fe0e5e7297258f193
Ot Rom <ot.rom01@estudiant.upf.edu>
2024-10-21 18:15:02 +0200

    First Attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,45 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+x = int(input("Enter a positive integer number: "))
+i = 1
+sum1 = 0
+sum2 = 0
+y = x - 1
+e = 0
+
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
 
-
+while i <= x :
+	if x % i == 0 :
+		sum1 = sum1 + 1 
+	i = i + 1
+while y > 0 :
+	i = 1
+	sum2 = 0
+	while i <= y :
+		if y % i == 0 :
+			sum2 = sum2 + 1
+		i = i + 1
+	if sum2 >= sum1 :
+		e = 1
+	y = y - 1
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
-if __name__ == "__main__" :
+if e == 1 :
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
-	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NO
+
+	print("Not Anti-prime")
+else :
+	print("Anti-prime")

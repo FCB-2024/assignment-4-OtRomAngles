a4bc1a98a1bd0a02ce6d7084fa20dac30b1bf1cf
Ot Rom <ot.rom01@estudiant.upf.edu>
2024-10-22 14:06:31 +0200

    20 attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,27 +1,43 @@
 import sys
-def main() :
-	def antiprime(num) :
-		sum1 = 0 
-		sum2 = 0
 
-		for i in range(1,num+1):
-			if num % i == 0:
-				sum1 = sum1 + 1
-		for i in range(1,num+1):
-			sum2 = 0
-			for y in range(1,i+1):
-				if i % y == 0 :
-					sum2 = sum2 + 1
-			if sum2 >= sum1 :
-				return("not anti-prime")
-		return("anti-prime")
-	num = int(sys.argv[1])
-	result = antiprime(num)
-	return result
-## DO NOT REMOVE THIS LINE BELOW:
+def main():
+    # Nested function to calculate if the number is anti-prime
+    def antiprimecalc(num):
+        divnum = 0  # Count of divisors of the given number
+        divi = 0  # Count of divisors for numbers smaller than num
+        
+        # Count the divisors of num
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divnum += 1
+
+        # Compare divisors of all numbers smaller than num
+        for i in range(1, num):
+            divi = 0
+            for j in range(1, i + 1):
+                if i % j == 0:
+                    divi += 1
+            # If any smaller number has more or equal divisors, return "not anti-prime"
+            if divi >= divnum:
+                return "not anti-prime"
+        
+        return "anti-prime"
+
+    # Ensure we have exactly one command-line argument and it's a valid integer
+    if len(sys.argv) != 2:
+        return f"error: {sys.argv[0]} <number>"
+
+    if not sys.argv[1].isdigit() or int(sys.argv[1]) <= 0:
+        return "Please provide a positive integer greater than 0."
+
+    num = int(sys.argv[1])
+    result = antiprimecalc(num)
+    return result
+
+# Main logic to run the program
 if __name__ == "__main__":
+    print(main())
 
-	print(main())
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6

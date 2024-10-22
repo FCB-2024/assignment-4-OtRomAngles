b3cda67307e433950c38aabe65daf224a7ffd183
Ot Rom <ot.rom01@estudiant.upf.edu>
2024-10-22 13:07:44 +0200

    12th attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,8 +1,5 @@
 def main() :
-	try:
-		number1 = int(input(""))
-	except EOFError:	
-		number1 = 0
+	number1 = int(input(""))
 	i = 1
 	sum1 = 0
 	sum2 = 0
@@ -31,8 +28,7 @@ def main() :
 
 ## DO NOT REMOVE THIS LINE BELOW:
 if __name__ == "__main__":
-	result = main()
-	print(result)
+	print(main())
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
